3a2ad136cf1351cecdba2d4aabdec7de4c92d0d5
betodasilva <jrs_robert@hotmail.com>
2017-10-21 22:14:11 -0300

    My custom setup with my configurations

--- wpdistillery/provision.sh
+++ wpdistillery/provision.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 #
 # WPDIstillery provisioning file
+# updated to use nightly build of WP-CLI
 #
-# Author: Flurin Dürst
+# Author: John Pitchko/Flurin Dürst
 # URL: https://wpdistillery.org
 #
-# File version 1.1.0
+# File version 1.1.1
 
 # update WP-CLI
 # since Scotch Box 3.0 (php7), we have to reinstall WP-CLI
+
 echo "== Update WP CLI (re-install) =="
 cd ../../../
 sudo rm usr/local/bin/wp
-sudo curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-sudo chmod +x wp-cli.phar
-sudo mv wp-cli.phar /usr/local/bin/wp
+sudo curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli-nightly.phar
+sudo chmod +x wp-cli-nightly.phar
+sudo mv wp-cli-nightly.phar /usr/local/bin/wp
 echo "WP-CLI Update successful"
 
 # run WPDistillery
